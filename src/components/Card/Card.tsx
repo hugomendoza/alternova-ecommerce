7b0e92af874bec9661f36db04cf58a7b79c9bf81
Hugo_Mendoza <hugo_mendoza_s@outlook.com>
2022-11-28 15:15:37 -0500

    feat: create card product component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,65 @@
+import { Button } from "../Button";
+import { StyledCard,
+  StyledCardProps,
+  StyledDiv,
+  StyledFigure,
+  StyledHeader,
+  StyledImage,
+  StyledInput,
+  StyledLabel,
+  StyledText,
+  StyledTitle
+} from "./Card.styles"
+
+interface CardProps extends StyledCardProps {
+  img: string
+  alt: string;
+  stock?: number;
+  onPress: () => void;
+}
+
+export const Card = (props:CardProps) => {
+
+  const {name, unit_price, type, img, onPress } = props
+
+  return (
+    <>
+      <StyledCard>
+        <StyledFigure>
+          <StyledImage
+            src={img}
+            alt={name}
+          />
+        </StyledFigure>
+
+        <StyledHeader>
+          <StyledTitle>{name}</StyledTitle>
+          <StyledDiv>
+            <StyledText>${unit_price}</StyledText>
+            <StyledText>
+              Cantidad:
+            </StyledText>
+          </StyledDiv>
+          <StyledDiv>
+            <StyledLabel type={type}>
+              {type}
+            </StyledLabel>
+            <StyledInput type="number" />
+          </StyledDiv>
+          <Button onPress={onPress} />
+        </StyledHeader>
+      </StyledCard>
+    </>
+  )
+}
+
+const defaultProps:CardProps = {
+  name: "",
+  img: "",
+  alt: "",
+  type: "",
+  stock: 0,
+  onPress: () => {},
+}
+
+Card.defaultProps = defaultProps;
